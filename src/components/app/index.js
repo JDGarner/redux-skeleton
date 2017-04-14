40879e21821a85c931426e320ac7c22992016d95
Jamie Garner <jamiegarner123@hotmail.co.uk>
2017-04-14 15:45:20 +0100

    random quote button, styling

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -1,34 +1,47 @@
 import React from 'react';
 import Quote from '../quote';
+import _ from 'lodash';
 
 class App extends React.Component {
 
   constructor() {
     super();
     this.state = {
-      quoteContent: '',
-      quoteAuthor: ''
+      quotes: [],
+      currentQuote: null
     };
   }
 
   componentDidMount() {
-    const url = 'http://localhost:3001/random-quote';
+    const url = 'http://localhost:3001/quotes';
 
     fetch(url)
       .then((res) => res.json())
       .then((data) => {
         this.setState({
-          quoteContent: data.content,
-          quoteAuthor: data.author
+          quotes: data,
+          currentQuote: _.sample(data)
         });
       });
   }
 
+  getNewQuote() {
+    this.setState({
+      currentQuote: _.sample(this.state.quotes)
+    });
+  }
+
   render() {
     return (
-      <Quote
-        content={this.state.quoteContent}
-        author={this.state.quoteAuthor} />
+      <div className="app">
+        <Quote quote={this.state.currentQuote} />
+        <button
+          className="btn btn-default"
+          type="submit"
+          onClick={this.getNewQuote.bind(this)}>
+          New Quote
+        </button>
+      </div>
     );
   }
 }
--- src/components/quote/index.js
+++ src/components/quote/index.js
@@ -3,11 +3,16 @@ import React from 'react';
 class Quote extends React.Component {
 
   render() {
-    return (
-      <div>
-        {this.props.content} - {this.props.author}
-      </div>
-    );
+    if (this.props.quote) {
+      return (
+        <div className="panel panel-default quote">
+          <div className="panel-body content">'{this.props.quote.content}'</div>
+          <div className="panel-footer author">{this.props.quote.author}</div>
+        </div>
+      );
+    } else {
+      return null;
+    }
   }
 }
 export default Quote;
